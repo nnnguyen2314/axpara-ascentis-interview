8618dbe0ff3fb1576da76589525368594519a313
Gerard Nguyen <78061928+nnnguyen2314@users.noreply.github.com>
2023-08-30 15:59:31 +0700

    Update number-of-weak-characters.js

--- number-of-weak-characters.js
+++ number-of-weak-characters.js
@@ -1,1 +1,18 @@
+/**
+ * @param {number[][]} properties
+ * @return {number}
+ */
+const numberOfWeakCharacters = function(properties) {
+    let weakCharaterCount = 0;
+    let maxDefense = 0;
 
+    properties.sort(([a1, d1 ], [a2, d2]) => a2 - a1 || d1 - d2);
+
+    for (let i = 0; i < properties.length; i++) {
+              if (properties[i][1] < maxDefense) {
+                  weakCharaterCount++;
+             }
+             maxDefense = Math.max(maxDefense, properties[i][1]);
+         }
+         return weakCharaterCount;
+};
